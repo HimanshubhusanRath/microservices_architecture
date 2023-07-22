ccfdd4e7e0234c293148b49558c6c89950d20743
himanshubhusan.rath <himanshubhusan.rath@landmarkgroup.com>
2023-07-23 00:18:22 +0530

    Update

--- ms-circuitbreaker-using-resillience4j/readme.md
+++ ms-circuitbreaker-using-resillience4j/readme.md
@@ -3,6 +3,7 @@
 In this project, the below features of Resillience4j are explored.
 * Circuit-breaker
 * Retry mechanism
+* Rate Limiter
 
 ### Circuit Breaker:
 * Here the circuit is opened if there is a failure percentage more than the threshold.
@@ -13,3 +14,16 @@ In this project, the below features of Resillience4j are explored.
 * Here, if the original source is not available/serviceable, then a retry is made to the original source after the configured amount of time delay.
 * After retrying for the configured retry count, if the original source is still not available, then the response is served from the fallback source.
 * Here the call is blocked, till the configured number of retries are made, before serving from the fallback source.
+
+### Rate Limiter:
+* Here, we define with in a configured span of time (e.g. 10 seconds), how many requests (e.g. 30) are allowed to be executed.
+* Only the allowed requests will get a chance to execute whereas the non-allowed requests will be served with the fall-back method.
+* Rate Limiter Algorithm:
+  * AtomicRateLimiter algorithm is used here.
+  * This can be considered a variation of 'Token Bucket' algorithm.
+  * This algorithm divides the entire duration into equal partitions of Nano-seconds (CYCLE) and applies rate limiting logic.
+* NOTE:
+  * The below events are triggered as part of this activity.
+      * Successfully acquired the permission [Approved request]
+      * Failure in acquiring the permission [Denied request]
+  * These events can be consumed/accessed by using the EventPublisher in RateLimiter.
--- ms-circuitbreaker-using-resillience4j/src/main/java/com/hr/circuitbreaker/controller/Controller.java
+++ ms-circuitbreaker-using-resillience4j/src/main/java/com/hr/circuitbreaker/controller/Controller.java
@@ -2,12 +2,17 @@ package com.hr.circuitbreaker.controller;
 
 import com.hr.circuitbreaker.dto.Activity;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -42,9 +47,28 @@ public class Controller {
         return activity.getActivity();
     }
 
+    @GetMapping("/activity/rate-limit")
+    @RateLimiter(name = "rateLimitMethod",fallbackMethod = "fallbackActivityForRateLimit")
+    public ResponseEntity<String> getActivityWithRateLimit(@RequestParam("id")final int id)
+    {
+        final long startTime = System.currentTimeMillis();
+        LOGGER.info("Request ID ["+id+"] Allowed to call API at : "+ Calendar.getInstance().getTime());
+        final ResponseEntity<Activity> response = restTemplate.getForEntity(REST_ENDPOINT,Activity.class);
+        final Activity activity = response.getBody();
+        LOGGER.info("Request ID ["+id+"] ---> Total Time Taken : "+(System.currentTimeMillis() - startTime) +" ms");
+        return ResponseEntity.ok(activity.getActivity());
+    }
+
     private String fallbackActivity(Throwable throwable)
     {
+        LOGGER.info(">>>>> FALLBACK");
         return "-- No activity available --";
+    }
+
+    private ResponseEntity<String> fallbackActivityForRateLimit(@RequestParam("id")final int id, Throwable throwable)
+    {
+        LOGGER.error("Request ID ["+id+"] >>>>> FALLBACK"+throwable.getLocalizedMessage());
+        return new ResponseEntity<String>("-- No activity available --", HttpStatus.BAD_REQUEST);
     }
 
 }
