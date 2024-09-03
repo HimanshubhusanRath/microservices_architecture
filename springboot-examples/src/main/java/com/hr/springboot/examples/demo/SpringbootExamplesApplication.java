aa12f98829f2c037042b038887e68ec14af87bc5
himanshubhusan.rath <himanshubhusan.rath@landmarkgroup.com>
2024-09-03 20:27:58 +0530

    event handling added

--- springboot-examples/src/main/java/com/hr/springboot/examples/demo/SpringbootExamplesApplication.java
+++ springboot-examples/src/main/java/com/hr/springboot/examples/demo/SpringbootExamplesApplication.java
@@ -2,8 +2,10 @@ package com.hr.springboot.examples.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringbootExamplesApplication {
 
 	public static void main(String[] args) {
